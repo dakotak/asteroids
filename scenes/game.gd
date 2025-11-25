c03cecf8693eec20bd930950590f62c3191270d3
dakotak <dakotak@outlook.com>
2025-11-25 05:56:48 -0800

    Add game over screen

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var hud: Control = $UI/HUD
 @onready var player: CharacterBody2D = $Player
+@onready var game_over_screen: Control = $UI/GameOverScreen
 
 @export var starting_lives := 3
 var lives : int :
@@ -17,13 +18,18 @@ func _ready() -> void:
 	lives = starting_lives
 
 
+func start_game() -> void:
+	game_over_screen.hide()
+	lives = starting_lives
+	# Show the ship
+
 func _ship_killed() -> void:
 	lives -= 1
 	
 
-
 func _game_over() -> void:
-	pass
+	game_over_screen.show()
+	player.hide()
 
 
 func _process(delta: float) -> void:
--- scenes/hud.gd
+++ scenes/hud.gd
@@ -3,6 +3,20 @@ extends Control
 @onready var score: Label = $Score :
 	set(val):
 		score.text = "SCORE: " + str(val)
+		
+var ship_life_icon_scene := preload("res://scenes/lives_bar_icon.tscn")
+@onready var lives_container: HBoxContainer = $VBoxContainer/LivesContainer
 
-func _ready() -> void:
-	pass
+var lives : int :
+	set(val):
+		lives = val
+		_update_lives_bar()
+
+func _update_lives_bar() -> void:
+	# Clear out all children
+	for c in lives_container.get_children():
+		c.queue_free()
+	
+	for na in range(lives):
+		var i = ship_life_icon_scene.instantiate()
+		lives_container.add_child(i)
